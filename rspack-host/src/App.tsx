5fec5203b032e7e7aca04d50706bcac04f504ef2
gnganapath <gnganapathi@gmail.com>
2026-03-26 07:27:41 +0530

    respack React monolitic host, remote app with tailwind, lint, prettier +typescript

---
+++ rspack-host/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild. on 26 mar 2026</p>
+    </div>
+  );
+};
+
+export default App;
